cf219c93f13138a9765cb81bfb8ea527c54d92dc
Kafva <kafva.one@gmail.com>
2025-03-03 07:05:41 +0100

    Add test files

---
+++ tests/files/arglists_input.rs
@@ -0,0 +1,5 @@
+impl AppBinding {
+    pub fn new(name: &str, vertex_entry_point: &str, fragment_entry_point: &str, vertices: Vec<Vertex>, config: &wgpu::SurfaceConfiguration, device: &wgpu::Device, pipeline_layout: &wgpu::PipelineLayout, primitive: wgpu::PrimitiveState, shader_module: &wgpu::ShaderModule) -> Self {
+    }
+}
+
---
+++ tests/files/arglists_input.zig
@@ -0,0 +1,8 @@
+const std = @import("std");
+
+fn build_tests(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, build_options: *std.Build.Step.Options) void {
+    _ = b;
+    _ = target;
+    _ = optimize;
+    _ = build_options;
+}
