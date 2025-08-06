411dfbed9bc23849bc70966a42bb17619a60acc2
Kafva <kafva.one@gmail.com>
2025-08-06 17:57:54 +0200

    Simplify check.sh

--- check.sh
+++ check.sh
@@ -19,7 +19,7 @@ ln -fns $TS_PATH/parser tests/parser
 
 # Run tests
 if [ $# = 0 ]; then
-    tests/tsst.nvim/tsst tests/*_test.lua || :
+    tests/tsst.nvim/tsst || :
     git checkout tests/files 2> /dev/null
 else
     tests/tsst.nvim/tsst $@
